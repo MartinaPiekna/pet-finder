07db9994eba7c8ef8d57c845fecccd084c98cf6d
Barbora Stundlova <b.stundlova@gmail.com>
2020-11-24 17:26:36 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,5 @@ import React, { useState } from 'react';
 import './footer.scss';
 
 export const Footer = () => {
-  return(
-  <>
-  <footer className="footer">
-    Footer text
-  </footer>
-  </>)
-}
+  return <footer className="footer">Footer text</footer>;
+};
