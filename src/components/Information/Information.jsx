77345e4247cf48ab80f38b7478e729c2961f768b
Barbora Stundlova <b.stundlova@gmail.com>
2020-11-25 17:20:18 +0100

    Information, styles not yet

---
+++ src/components/Information/Information.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './information.scss';
+
+export const Information = () => {
+  const [hidden, setHidden] = useState(false);
+ 
+
+  return (
+    <>
+      <div className="information">
+        <h1 className="information__headline">Co ještě můžete udělat?</h1>
+        <div className="information__tip">
+          <div className="information__button">
+            <button
+              className={hidden? "information__button--nonactive":"information__button--active"} 
+              onClick={() => {
+                setHidden(false);
+                
+              }}
+            >
+              Našlo se zvíře
+            </button>
+            <button
+              className={hidden? "information__button--active":"information__button--nonactive"}
+              onClick={() => {
+                setHidden(true);
+                
+              }}
+            >
+              Hledám zvíře
+            </button>
+          </div>
+
+          <div className={hidden ? 'information__sectionHidden' : 'information__sectionShown'}>
+            PRVNI Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Omnis maxime pariatur ipsa error dicta nulla facilis enim eaque nam
+            explicabo a mollitia ea velit accusantium, delectus aliquid hic
+            inventore consequuntur.
+          </div>
+          <div className={hidden ? 'information__sectionShown' : 'information__sectionHidden'}>
+            DRUHY Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+            reiciendis repellendus veritatis ad, harum sint! Molestias cumque,
+            sapiente odit, velit quam ut iste harum cupiditate eligendi vero a
+            deleniti atque.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import './style.scss';
 import { Navigation } from './components/Navigation/Navigation.jsx';
 import { Form } from './components/Form/Form.jsx';
 // import { About } from './components/About/About.jsx';
-// import { Information } from './components/Information/Information.jsx';
+import { Information } from './components/Information/Information.jsx';
 import { Footer } from './components/Footer/Footer.jsx';
 
 const App = () => {
@@ -20,7 +20,7 @@ const App = () => {
           <Switch>
             <Route exact path="/" /*component={Home}*/ />
             <Route path="/new" component={Form} />
-            <Route path="/info" /*component={Information}*/ />
+            <Route path="/info" component={Information} />
             <Route path="/about" /*component={About}*/ />
           </Switch>
         </main>
