87662d6f4456c1e77a7c346cbdc2e325b9a13215
Martina Hytychová <martina.hyt@gmail.com>
2020-11-28 11:27:36 +0100

    komponenta Home, mapa ve formuláři

--- src/components/MapForm.jsx/MapForm.jsx
+++ src/components/MapForm.jsx/MapForm.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  GoogleMap,
+  withGoogleMap,
+  withScriptjs,
+  Marker,
+} from 'react-google-maps';
+import logo from '../../assets/img/location.svg';
 
-export const MapForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+export const MapForm = withScriptjs(
+  withGoogleMap((props) => {
+    const [lostLocation, setLostLocation] = useState([]);
+    return (
+      <>
+        <GoogleMap defaultZoom={8} defaultCenter={{ lat: 50.0, lng: 15.5 }}>
+          {props.isMarkerShown && (
+            <Marker position={{ lat: -34.397, lng: 150.644 }} />
+          )}
+        </GoogleMap>
+      </>
+    );
+  }),
+);
