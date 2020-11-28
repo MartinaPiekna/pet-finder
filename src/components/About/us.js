af9650a857db3d2cd6238826eb648db49c22d2a6
Barbora Stundlova <b.stundlova@gmail.com>
2020-11-28 09:36:36 +0100

    komponenty information

---
+++ src/components/About/us.js
@@ -0,0 +1,28 @@
+"use strict";
+import martinaPic from "./img/martina.png"
+import barboraPic from "./img/barbora.png"
+
+export const usInfo = [
+  {about: "O Martině",
+  profilePic: "martinaPic", //oddělat uvozovy
+  name: "martina",
+  linkedin: "https://www.linkedin.com/in/martina~hytychova/",
+  git: "https://github.com/MartinaHytychova",
+  fb: "https://www.facebook.com/martina.hytychova",
+  fullName: "Martina Hytychová",
+  freeTime: "programování, webdesignu, cestování a výletům, Netflixu a spánku",
+  motivation: "Vždy jsem měla zájem o vše živé. Lidem se jejich zvířecí parťáci ztrácí každý den a spousta lidí také zatoulaná zvířata nachází. Obě skupiny často neví, jak postupovat a informace jsou pak roztříštěné různě ve FB skupinách, u městské policie,útulcích apod. Tímto projektem bych ráda tyto informace centralizovala na jedno místo s mapou a informacemi.",
+  common: "Máme společný zájem o zvířata, obě máme nebo jsme měly mazlíčky a chceme tvořit smysluplný projekt.",
+},
+{about: "O Barboře",
+  profilePic:"barboraPic",  //tady taky
+  name: "barbora",
+  linkedin: "",
+  git: "",
+  fb: "",
+  fullName: "Barbora Stündlová",
+  freeTime: "doplnit",
+  motivation: "doplnit",
+  common: "doplnit",
+}
+]
