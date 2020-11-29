b259e4296099af580e97318122abe0c8bf586e0e
Martina Hytychová <martina.hyt@gmail.com>
2020-11-29 17:11:37 +0100

    validace inputů formuláře

--- src/components/Shelters/Shelters.jsx
+++ src/components/Shelters/Shelters.jsx
@@ -7,12 +7,12 @@ export const Shelters = () => {
     <div className="shelters">
       {shelters.map((shelter) => (
         <div className="shelters__card" key={shelter.id}>
-          <h2>{shelter.nazev}</h2>
-          <h3>{shelter.okres}</h3>
-          <p>{shelter.adresa}</p>
-          <p>{shelter.telefon}</p>
-          <p>{shelter.email}</p>
-          <p>{shelter.provozni_doba}</p>
+          <h2 className="shelters__name">{shelter.nazev}</h2>
+          <h3 className="shelters__location">{shelter.okres}</h3>
+          <p className="shelters__address">{shelter.adresa}</p>
+          <p className="shelters__phone">{shelter.telefon}</p>
+          <p className="shelters__email">{shelter.email}</p>
+          <p className="shelters__open-hours">{shelter.provozni_doba}</p>
         </div>
       ))}
     </div>
